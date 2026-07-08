8429d025a66c8b9a0b56b9931dc8a78382e4a702
smyshlyaev.s9 <smyshlyaev.s9@rwb.ru>
2026-07-08 18:00:06 +0300

    feature(PMA-12): lesson 4: feed screen

--- app/src/androidTest/java/com/example/kaspresso_learning/screens/FeedScreen.kt
+++ app/src/androidTest/java/com/example/kaspresso_learning/screens/FeedScreen.kt
@@ -1,4 +1,61 @@
 package com.example.kaspresso_learning.screens
 
-object FeedScreen {
+import com.example.kaspresso_learning.Tags
+import io.github.kakaocup.compose.node.element.ComposeScreen
+import io.github.kakaocup.compose.node.element.KNode
+
+object FeedScreen : ComposeScreen<FeedScreen>(){
+
+    val feedTitle : KNode = child {
+        hasTestTag(Tags.FEED_TITLE)
+    }
+
+    fun feedPost(index: Int, function: () -> Unit) : KNode = child {
+        hasTestTag("${Tags.FEED_POST}_$index")
+    }
+
+    val feedPostAvatar : KNode = child {
+        hasTestTag(Tags.FEED_POST_AVATAR)
+    }
+
+    val feedPostAuthorName: KNode = child {
+        hasTestTag(Tags.FEED_POST_AUTHOR_NAME)
+    }
+
+    val feedPostDate: KNode = child {
+        hasTestTag(Tags.FEED_POST_DATE)
+    }
+
+    val feedPostTeaName: KNode = child {
+        hasTestTag(Tags.FEED_POST_TEA_NAME)
+    }
+
+    val feedPostTeaType: KNode = child {
+        hasTestTag(Tags.FEED_POST_TEA_TYPE)
+    }
+
+    val feedPostWeightGrams: KNode = child {
+        hasTestTag(Tags.FEED_POST_WEIGHT)
+    }
+
+    val feedPostVolumeMl: KNode = child {
+        hasTestTag(Tags.FEED_POST_VOLUME)
+    }
+
+    val feedPostVessel: KNode = child {
+        hasTestTag(Tags.FEED_POST_VESSEL)
+    }
+
+    val feedScreenFabButton: KNode = child {
+        hasTestTag(Tags.FEED_FAB)
+    }
+
+    val feedProfileButton: KNode = child {
+        hasText("Профиль")
+    }
+
+    val feedButton: KNode = child {
+        hasText("Лента")
+    }
+
 }
--- app/src/androidTest/java/com/example/kaspresso_learning/steps/FeedSteps.kt
+++ app/src/androidTest/java/com/example/kaspresso_learning/steps/FeedSteps.kt
@@ -1,4 +1,67 @@
 package com.example.kaspresso_learning.steps
 
+import com.example.kaspresso_learning.screens.FeedScreen
+
 object FeedSteps {
+
+    fun clickFeedScreenFabButton() {
+        FeedScreen {
+            feedScreenFabButton {
+                assertIsDisplayed()
+                performClick()
+            }
+        }
+    }
+
+    fun clickFeedButton() {
+        FeedScreen {
+            feedButton {
+                assertIsDisplayed()
+                performClick()
+            }
+        }
+    }
+
+    fun clickProfileButton() {
+        FeedScreen {
+            feedProfileButton {
+                assertIsDisplayed()
+                performClick()
+            }
+        }
+    }
+
+    fun asserCardParamsDisplayed(index: Int) {
+        FeedScreen {
+            feedPost(index) {
+                feedPostAuthorName {
+                    assertIsDisplayed()
+                }
+                feedPostDate{
+                    assertIsDisplayed()
+                }
+                feedPostTeaName{
+                    assertIsDisplayed()
+                }
+
+            }
+        }
+
+    }
+
+    fun upScroll() {
+        FeedScreen {
+            performTouchInput {
+                upScroll()
+            }
+        }
+    }
+
+    fun downScroll() {
+        FeedScreen {
+            performTouchInput {
+                downScroll()
+            }
+        }
+    }
 }
