13bda965461ae97dcad130d09b98ef837959b20e
smyshlyaev.s9 <smyshlyaev.s9@rwb.ru>
2026-07-14 17:41:18 +0300

    feature(PMA-12): lesson 6 : task completed

--- app/src/androidTest/java/com/example/kaspresso_learning/screens/FeedScreen.kt
+++ app/src/androidTest/java/com/example/kaspresso_learning/screens/FeedScreen.kt
@@ -6,13 +6,13 @@ import io.github.kakaocup.compose.node.element.KNode
 import androidx.compose.ui.test.hasTestTag as matchTag
 import androidx.compose.ui.test.hasText as matchText
 
-object FeedScreen : ComposeScreen<FeedScreen>(){
+object FeedScreen : ComposeScreen<FeedScreen>() {
 
-    val feedScreenFabButton : KNode = child {
+    val feedScreenFabButton: KNode = child {
         hasTestTag(Tags.FEED_FAB)
     }
 
-    fun teaNameOnCard(authorName: String): KNode =child {
+    fun teaNameOnCard(authorName: String): KNode = child {
         hasTestTag(Tags.FEED_POST_TEA_NAME)
         hasAnySibling(matchTag(Tags.FEED_POST_AUTHOR_NAME) and matchText(authorName))
     }
